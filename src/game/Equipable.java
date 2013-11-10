2aa88e6f81f3eebd6e53a26969b7a88f794a8f28
masilan <robomas@gmail.com>
2013-11-09 21:59:36 -0800

    Combined weapons, shields, armor into one enum called Equipable. Created status enum.

---
+++ src/game/Equipable.java
@@ -0,0 +1,53 @@
+package game;
+
+public enum Equipable {
+    QUICKBLADE("Quickblade", 9, 1, 0, 0, 1),
+    DAGGER("Dagger", 8, 2, 0, 0, 3),
+    SHORTSWORD("Short Sword", 7, 3, 0, 0, 5),
+    RAPIER("Rapier", 6, 4, 0, 0, 7),
+    MAUL("Maul", 5, 5, 0, 0, 9),
+    SWORD("Sword", 4, 6, 0, 0, 7),
+    WARAXE("War Axe", 3, 7, 0, 0, 5),
+    BROADSWORD("Broad Sword", 2, 8, 0, 0, 3),
+    EXECAXE("Executioner's Axe", 1, 9, 0, 0, 1),
+    STARWEAVE("Starweave Armor", 0, 0, 0, 7, 1),
+    CLOTH("Cloth Armor", 0, 0, 1, 5, 3),
+    LEATHER("Leather Armor", 0, 0, 2, 3, 5),
+    HIDE("Hide Armor", 0, 0, 3, 1, 7),
+    SCALE("Scale Armor", 0, 0, 5, -1, 7),
+    STEEL("Steel Armor", 0, 0, 8, -3, 5),
+    LEAD("Lead Armor", 0, 0, 11, -5, 3),
+    DIAMOND("Diamond Armor", 0, 0, 14, -7, 1),
+    BUCKLER("Buckler Shield", 0, 0, 2, 3, 2),
+    LIGHT("Light Shield", 0, 0, -1, 5, 6),
+    HEAVY("Heavy Shield", 0, 0, -3, 8, 2);
+  
+    private String name;
+    public String getName() { return name; }
+    
+    private int attack;
+    public int getAttack() { return attack; }
+    
+    private int damage;
+    public int getDamage() { return damage; }
+    
+    private int defense;
+    public int getDefense() { return defense; }
+    
+    private int evasion;
+    public int getEvasion() { return evasion; }
+    
+    private int rarity;
+    public int getRarity() { return rarity; }
+    
+    Equipable(String name, int attack, int damage, int defense, int evasion, int rarity)
+    {
+        this.name = name;
+        this.attack = attack;
+        this.damage = damage;
+        this.defense = defense;
+        this.evasion = evasion;
+        this.rarity = rarity;
+    }
+
+}
---
+++ src/game/Status.java
@@ -0,0 +1,54 @@
+package game;
+
+import asciiPanel.AsciiPanel;
+import java.awt.Color;
+
+public enum Status {
+    FREEZE("Frozen", AsciiPanel.brightBlue, 0, -3, 0, -2, 0, 3),
+    BURN("Burned", AsciiPanel.yellow, 4, 0, 0, 0, 0, 1),
+    POISON("Poisoned", AsciiPanel.green, -2, 0, 0, 0, 0, 6),
+    CONFUSE("Confused", AsciiPanel.brightYellow, 0, -2, 0, -2, 0, 3),
+    WEAKEN("Weakened", AsciiPanel.brightBlack, 0, -1, -3, -1, -3, 2),
+    BUFF("Strengthened", AsciiPanel.blue, 0, 1, 1, 1, 1, 6),
+    BERSERK("Berserk", AsciiPanel.brightRed, 0, 3, 6, 0, 0, 5),
+    EXHAUST("Exhausted", AsciiPanel.magenta, 0, -1, -2, 0, -1, 0),
+    BLEED("Bleeding", AsciiPanel.red, -1, 0, 0, 0, 0, 8),
+    HEAL("Healed", AsciiPanel.brightGreen, 10, 0, 0, 0, 0, 0),
+    ROT("Rotting", AsciiPanel.black, -3, -2, -2, -2, -2, 5);
+    
+    private String description;
+    public String getDescription() { return description; }
+    
+    private Color color;
+    public Color getColor() { return color; }
+    
+    private int health;
+    public int getHealth() { return health; }
+    
+    private int attack;
+    public int getAttack() { return attack; }
+    
+    private int damage;
+    public int getDamage() { return damage; }
+    
+    private int evasion;
+    public int getEvasion() { return evasion; }
+    
+    private int defense;
+    public int getDefense() { return defense; }
+
+    private int duration;
+    public int getDuration() { return duration; }
+    
+    Status(String description, Color color, int health, int attack, int damage, int evasion, int defense, int duration)
+    {
+        this.description = description;
+        this.color = color;
+        this.health = health;
+        this.attack = attack;
+        this.damage = damage;
+        this.evasion = evasion;
+        this.defense = defense;
+        this.duration = duration;
+    }
+}
