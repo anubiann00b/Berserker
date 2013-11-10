416061f012bdf0ddf68b2980ded376bc556da513
Shreyas <skraman1999@gmail.com>
2013-11-10 07:44:24 -0800

    Added base Equipables.

--- src/game/Equipable.java
+++ src/game/Equipable.java
@@ -14,6 +14,7 @@ public enum Equipable {
     WARAXE("War Axe", 3, 7, 0, 0, 5, '\\', Color.LIGHT_GRAY, 0),
     BROADSWORD("Broad Sword", 2, 8, 0, 0, 3, '\\', AsciiPanel.brightBlue, 0),
     EXECAXE("Executioner's Axe", 1, 9, 0, 0, 1, '\\', AsciiPanel.blue, 0),
+    SHIRT("Shirt", 0,0,0,0,0, '(', Color.LIGHT_GRAY, 1),
     STARWEAVE("Starweave Armor", 0, 0, 0, 7, 1, '(', Color.LIGHT_GRAY, 1),
     CLOTH("Cloth Armor", 0, 0, 1, 5, 3, '(', AsciiPanel.brightBlue, 1),
     LEATHER("Leather Armor", 0, 0, 2, 3, 5, ']', Color.LIGHT_GRAY, 1),
@@ -22,6 +23,7 @@ public enum Equipable {
     STEEL("Steel Armor", 0, 0, 8, -3, 5, '}', Color.LIGHT_GRAY, 1),
     LEAD("Lead Armor", 0, 0, 11, -5, 3, '}', AsciiPanel.brightBlue, 1),
     DIAMOND("Diamond Armor", 0, 0, 14, -7, 1, '}', AsciiPanel.blue, 1),
+    LONGSLEEVES("Long Sleeves", 0,0,0,0,0, '*', Color.LIGHT_GRAY, 1),
     BUCKLER("Buckler", 0, 0, 2, 3, 2, '*', Color.LIGHT_GRAY, 2),
     LIGHT("Light Shield", 0, 0, -1, 5, 6, '*', AsciiPanel.brightBlue, 2),
     HEAVY("Heavy Shield", 0, 0, -3, 8, 2, '*', AsciiPanel.blue, 2);
