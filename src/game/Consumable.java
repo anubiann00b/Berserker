8102b4696d0bb4825fd68b6c9c0b4a63af07c874
masilan <robomas@gmail.com>
2013-11-09 22:50:06 -0800

    Added Consumable Enum which characterizes potions with status effects from the Status enum.

---
+++ src/game/Consumable.java
@@ -0,0 +1,33 @@
+package game;
+
+public enum Consumable {
+    FIRE("Potion of Fire", Status.BURN, 5),
+    CHILL("Potion of Chill", Status.FREEZE, 5),
+    Poison("Potion of Poison", Status.POISON, 3),
+    CONFUSION("Potion of Confusion", Status.CONFUSE, 6),
+    WEAKNESS("Potion of Weakness", Status.WEAKEN, 5),
+    STRENGTH("Potion of Strength", Status.BUFF, 6),
+    BERSERK("Potion of Berserk", Status.BERSERK, 6),
+    //CURE("Potion of Cure", Status.HEAL, 1),
+    HEALING("Potion of Healing", Status.HEAL, 1),
+    ROT("Potion of Rot", Status.ROT, 9);
+    
+     private String name;
+    public String getName() { return name; }
+    
+    private Status status;
+    public Status getStatus() { return status; }
+    
+    private int rarity;
+    public int getRarity() { return rarity; }
+    
+    
+    Consumable(String name, Status status, int rarity)
+    {
+        this.name = name;
+        this.status = status;
+        this.rarity = rarity;
+    }
+    
+    
+}
