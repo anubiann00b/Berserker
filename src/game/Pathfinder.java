a4768c6c4ef7f62a7c2e65bc2fa19e7675e94436
s-ramansh <s-ramansh@54L1808-07.it.bsd405.org>
2013-11-15 15:52:00 -0800

    Doing A* Pathfinding.

--- src/game/Pathfinder.java
+++ src/game/Pathfinder.java
@@ -6,11 +6,13 @@ public class Pathfinder {
     public static void findPath(Point start, Point end) {
         ArrayList<Node> open = new ArrayList<Node>();
         ArrayList<Node> closed = new ArrayList<Node>();
-        closed.add(new Node(start));
-        for (Node n : new Node(start).getNeighbors()) {
+        Node focusNode = new Node(start);
+        closed.add(focusNode);
+        for (Node n : focusNode.getNeighbors()) {
             open.add(n);
             n.setG(n.getParent().getG()+1);
-            //n.setH(Math.max(Math.abs(n.getX()-end.getX()),Math.max(Math.abs(n.getX()-end.getX()))));
+            n.setH(Math.max(Math.abs(n.getX()-end.getX()),Math.abs(n.getX()-end.getX())));
+            n.setF(n.getG()+n.getH());
         }
     }
 }
